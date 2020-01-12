cb07d9a5f27627b42933b9cd02c7355f3f35fc05
405530042 <32125956+405530042@users.noreply.github.com>
2020-01-12 22:03:12 +0800

    0112修改pdo bug

--- login.php
+++ login.php
@@ -2,27 +2,31 @@
 <?php
 require 'connect.php';
 //include("connect.php");                                               
-$account = $_POST['account'];
-$password = $_POST['password'];
-
-$sql="SELECT * FROM user where account='$account' AND password='$password'";
-
+$account = (isset($_POST['account']))?$_POST['account']:"";
+$password = (isset($_POST['password']))?$_POST['password']:"";
+// $sql="SELECT * FROM user where account='$account' AND password='$password'";
+$sql="SELECT * FROM user where account LIKE :account AND password LIKE :password";
 $sth = $pdo->prepare($sql);
-$sth->execute(array($account, $password));
-$result = $sth->fetch(PDO::FETCH_OBJ);
+// $sth->execute(array($account, $password));
+$sth->execute(array(':account' => $account, ':password' => $password));
+// if($result)
+// {
+//         header('todolist.php');
+//         echo "登入成功";
+//         $_SESSION['account'] = $result['account'];
+//         $_SESSION['auth'] = $result['auth'];
+// }
+// else
+// {
+//         echo"登入失敗";
+// }
 
-//$result=mysqli_query($connect,$sql_query_login);
-if($result)
-{
-        header('todolist.php');
-        echo "登入成功";
-        $_SESSION['account'] = $result['account'];
-        $_SESSION['auth'] = $result['auth'];
+if($sth->execute(array(':account' => $account, ':password' => $password))){
+    echo "succ";
+    $result = $sth->fetch();
+    print($result['account']);
 }
-else
-{
-        echo"登入失敗";
+else{
+    echo $sth->error;
 }
-
-
 ?>
