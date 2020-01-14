42cfad87c18a281ae42477bdf62dea9c01851891
yianhsiung <yianhsiung@gmail.com>
2020-01-14 23:24:39 +0800

    0114

--- login.php
+++ login.php
@@ -1,7 +1,7 @@
 <?php session_start(); ?>
 <?php
 require 'connect.php';
-//include("connect.php");                                               
+                                              
 $account = (isset($_POST['account']))?$_POST['account']:"";
 $password = (isset($_POST['password']))?$_POST['password']:"";
 
@@ -10,7 +10,7 @@ $password = (isset($_POST['password']))?$_POST['password']:"";
 $sql="SELECT * FROM user where account LIKE :account AND password LIKE :password";
 
 $sth = $pdo->prepare($sql);
-//$sth->execute(array(':account' => $account, ':password' => $password));
+
 if($sth->execute(array(':account' => $account, ':password' => $password)))
 {
         header('todolist.php');
