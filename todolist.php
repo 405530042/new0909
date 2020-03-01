7c27f8df7234b425e85d3ec5a6a75ceeb9f7b952
yianhsiung <yianhsiung@gmail.com>
2020-03-01 20:50:04 +0800

    todolist修改、網頁menu

--- todolist.php
+++ todolist.php
@@ -1,30 +1,51 @@
 <?php session_start(); ?>
 <?php
-/**第3~10就不用了 直接include connect.php */
-// $host="127.0.0.1";
-// $user="root";
-// $pasd="";
-// $database="todo";
-// $db = mysqli_connect($host,$user,$pasd,$database);
 
-// mysqli_query($db,'set names utf8');
+$host="127.0.0.1";
+$user="root";
+$pasd="";
+$database="todo";
+$db = mysqli_connect($host,$user,$pasd,$database);
+
+mysqli_query($db,'set names utf8');
+$task="";
+$update=false;
+$id=0;
 
 
 
-//     if(isset($_POST['submit'])){
-//     $task=$_POST['task'];
+    if(isset($_POST['submit'])){
+    $task=$_POST['task'];
 
-//     mysqli_query($db,"INSERT INTO tasks (task) VALUES ('$task')");
+    mysqli_query($db,"INSERT INTO tasks (task) VALUES ('$task')");
     
-//     }
+    }
 
-//     if(isset($_GET['del_task'])){
-//         $id=$_GET['del_task'];
-//         mysqli_query($db,"DELETE FROM tasks WHERE id=$id");
+    if(isset($_GET['del_task'])){
+        $id=$_GET['del_task'];
+        mysqli_query($db,"DELETE FROM tasks WHERE id=$id");
         
-//     }
+    }
 
-//     $tasks=mysqli_query($db,"SELECT * FROM tasks");
+    if(isset($_GET['edit'])){
+        $id=$_GET['edit'];
+        $update=true;
+        $result=mysqli_query($db,"SELECT * FROM tasks WHERE id=$id");
+        if($result->num_rows){
+            $row = $result->fetch_array();
+            $task=$row['task'];
+        }
+
+    }
+
+    if(isset($_POST['update'])){
+        $id=$_POST['id'];
+        $task=$_POST['task'];
+        mysqli_query($db,"UPDATE tasks SET task='$task' WHERE id=$id");
+        //header('location : todolist.php');
+    }
+
+    $tasks=mysqli_query($db,"SELECT * FROM tasks");
 
     /*if(isset($_SESSION))
     {
@@ -50,9 +71,15 @@
             <h2>To Do List</h2>
         </div>  
             
-            <form  action="todolist.php" method="post">
-                <input type="text" class="task_input"  name="task" placeholder="輸入待辦事項" required>
+            <form  action="todolist.php" method="POST">
+                <input type="hidden" name="id" value="<?php echo $id; ?>">
+                <input type="text" class="task_input"  name="task" value="<?php echo $task; ?>" placeholder="輸入待辦事項" required>
+                <?php if($update==true):
+                ?>
+                <button type="submit" class="add_btn" name="update">修改</button>
+                <?php else: ?>
                 <button type="submit" class="add_btn" name="submit">新增</button>
+                <?php endif; ?>
             </form>
 
             <table>
@@ -66,17 +93,25 @@
 
                 <tbody>
                 
-                   
+                    <?php  $i=1; while ($row = mysqli_fetch_array($tasks)){ ?>
                     <tr>
                     
-                       
+                        <td class="task"><?php echo $row['task'];?></td>
+                        <td class="delete">
+                            <a href="todolist.php?del_task=<?php echo $row['id'];?>">x </a>
+                            
+                            <a href="todolist.php?edit=<?php echo $row['id'];?>">編輯</a>
+                        </td>
                         
-                    </tr> 
+                        
+                    </tr>
+                    <?php $i++;  } ?> 
                 </tbody>
             </table>
-        <?php if(isset($_SESSION['auth']))//isset()return的值是true or false所以 ==數字 會false
+        <?php /*if(isset($_SESSION['auth']))
         {
             if($_SESSION['auth']==3){
+        
             ?>
             
 
@@ -84,14 +119,15 @@
 
             </div>
         <?php
-            }
         } 
+        
         else{
             ?>
             <a href="">註冊帳號</a><?php echo $_SESSION['auth'] ?></div>
         
         <?php
         }
+    }*/
         ?>
     
     </body>
